84aae6a62b7432e5713e88bb8c4f40f72d746eaf
patrikfr Patrik Fredriksson <patrik@snart.nu>
2014-10-21 09:11:31 +0200

    Added support for CF/Bluemix

--- src/main/java/se/citerus/notes/paas/CloudFoundry.java
+++ src/main/java/se/citerus/notes/paas/CloudFoundry.java
@@ -1,19 +1,25 @@
 package se.citerus.notes.paas;
 
+import com.jayway.jsonpath.JsonPath;
+
 public class CloudFoundry implements Connector {
 
-   @Override
-   public boolean available() {
-      return false;
-   }
+    @Override
+    public boolean available() {
+        return System.getenv("VCAP_SERVICES") != null;
+    }
 
-   @Override
-   public String url() {
-      return "TODO";
-   }
+    @Override
+    public String url() {
 
-   @Override
-   public String db() {
-      return "TODO";
-   }
+        String json = System.getenv("VCAP_SERVICES");
+
+        return JsonPath.read(json, "$.['mongodb-2.2'][0].credentials.url");
+    }
+
+    @Override
+    public String db() {
+        return "db";
+    }
+
 }
